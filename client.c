69698ca55e1ae1bf41a312b597380ab6735c7948
biraj-phuyal <birajphuyal11@gmail.com>
2025-07-16 16:53:15 +0100

    starting mintialk

---
+++ client.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 16:27:49 by biphuyal          #+#    #+#             */
+/*   Updated: 2025/07/16 16:51:25 by biphuyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact)
+{
+	
+}
---
+++ minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 16:46:34 by biphuyal          #+#    #+#             */
+/*   Updated: 2025/07/16 16:50:16 by biphuyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+
+int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
+pid_t getpid(void);
+
+#endif
